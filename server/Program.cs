5613a548a21416692f286950aaf0b2a4129d9ac5
sorwat <sebastianorwat9@gmail.com>
2020-05-07 02:09:33 +0200

    Initial frontend commit

---
+++ server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace CodeMeasurement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeasurementsDemo.main();
+        }
+    }
+}
---
+++ web-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
